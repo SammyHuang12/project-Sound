5d875483a2b0ee05f9df0ab3610859b85e740773
SammyHuang12 <jessie_hkimo@yahoo.com.tw>
2024-09-20 11:34:50 +0800

    test

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 
 // import Swiper bundle with all modules installed
-import Swiper from 'swiper/bundle';
+// import Swiper from 'swiper/bundle';
 
 // import styles bundle
-import 'swiper/css/bundle';
+// import 'swiper/css/bundle';
 
 // init Swiper:
 
