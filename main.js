f33c18d8c4a7352fa266dd8b9f5e3587ab0955bf
SammyHuang12 <jessie_hkimo@yahoo.com.tw>
2024-10-12 19:45:08 +0800

    index頁面修改

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 
 console.log("Hello world!");
 
-const swiper = new Swiper('.swiper', {
+const bannerSwiper = new Swiper('.banner-swiper', {
     // Optional parameters
     direction: 'horizontal',
     speed: 2000,
@@ -20,11 +20,41 @@ const swiper = new Swiper('.swiper', {
 
     autoplay: {
         delay: 5000,
-      },
+    },
   
     effect: 'fade',
     fadeEffect: {
         crossFade: true,
     },
 
-  });
+});
+
+const coustomerSwiper = new Swiper('.customer-swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    speed: 2000,
+    loop: true,
+
+    autoplay: {
+        delay: 3000,
+    },
+  
+    spaceBetween: 32,
+  	slidesPerView: 2,
+
+  	breakpoints: {
+  	  	576: {
+  	  	  //當螢幕寬度大於等於576
+  	  	  slidesPerView: 2,
+  	  	},
+  	  	992: {
+  	  	  //當螢幕寬度大於等於992
+  	  	  slidesPerView: 4,
+  	  	},
+  	  	1200: {
+  	  	  //當螢幕寬度大於等於1200
+  	  	  slidesPerView: 6,
+  	  	},
+  	},
+
+});
